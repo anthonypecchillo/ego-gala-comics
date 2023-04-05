21d9d4db5f1ec916402210506a07094414944b26
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-04-05 10:49:03 -0700

    Init theme and Global Styles component

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,14 @@
+// styles/GlobalStyle.ts
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Arial', sans-serif;
+    background-color: #f0f0f0;
+    background-color: #ddd;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+// styles/theme.ts
+const theme = {
+  colors: {
+    primary: '#333',
+    secondary: '#a0a',
+  },
+};
+
+export default theme;
