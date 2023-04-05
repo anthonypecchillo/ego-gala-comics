17759e40c0759a0edd8b36474909fd0d6c775a01
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-04-05 10:59:16 -0700

    Create Panel model

---
+++ db/models/Panel.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IPanel extends Document {
+  comic_id: string;
+  panel_number: number;
+  image_url: string;
+  transcription: string;
+}
+
+const PanelSchema: Schema = new Schema({
+  comic_id: { type: Schema.Types.ObjectId, ref: 'Comic', required: true },
+  panel_number: { type: Number, required: true },
+  image_url: { type: String, required: true },
+  transcription: { type: String, required: true },
+});
+
+export default mongoose.model<IPanel>('Panel', PanelSchema);
