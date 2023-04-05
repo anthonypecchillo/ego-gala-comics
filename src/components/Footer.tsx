7f4a44bb987e7ce725325323719dd401a386a7a2
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-04-05 10:48:16 -0700

    Init Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: #574177;
+  background-color: #333;
+  bottom: 0;
+  padding: 45px;
+
+  @media (max-width: 480px) {
+    padding: 20px;
+    position: sticky;
+  }
+`;
+
+const FooterLink = styled.a`
+  color: #fff;
+  margin: 0 10px;
+  text-decoration: none;
+  &:hover {
+    color: #ccc;
+    color: #73E10A;
+  }
+`;
+
+const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      <FooterLink href="#">Link 1</FooterLink>
+      <FooterLink href="#">Link 2</FooterLink>
+      <FooterLink href="#">Link 3</FooterLink>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
