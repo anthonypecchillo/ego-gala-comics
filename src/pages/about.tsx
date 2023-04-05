a0d96c4256ce466f6e8a9bc29828b9fac4052b31
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-04-05 10:49:38 -0700

    Init About page

---
+++ src/pages/about.tsx
@@ -0,0 +1,28 @@
+// components/About.tsx
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+// import Hero from '../components/Hero';
+// import GridSection from '../components/GridSection';
+import NewsletterForm from '../components/NewsletterForm';
+import GlobalStyle from '../styles/GlobalStyle';
+import theme from '../styles/theme';
+
+const About: React.FC = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      {/* <Router> */}
+      <GlobalStyle />
+      <Navbar />
+      <div>About page</div>
+      <NewsletterForm />
+      <Footer />
+      {/* </Router> */}
+    </ThemeProvider>
+  );
+};
+
+
+
+export default About;
