c5e5f56042bb28803ad372c2691dbab622b8e206
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-04-05 11:06:33 -0700

    Add GET routes and request handlers for /comics and /comics:id

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,40 @@
+// server/index.ts
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
+import db from '../db';
+import Comic, { IComic } from '../db/models/Comic';
+import Panel, { IPanel } from '../db/models/Panel';
 
 const app = new Koa();
 const router = new Router();
 
-// Routes will be added here
+router.get('/comics', async (ctx) => {
+  try {
+    const comics: IComic[] = await Comic.find().populate('panels');
+    ctx.body = comics;
+  } catch (err) {
+    console.error(err);
+    ctx.status = 500;
+    ctx.body = { error: 'An error occurred while fetching comics.' };
+  }
+});
+
+router.get('/comics/:id', async (ctx) => {
+  try {
+    const comic: IComic | null = await Comic.findById(ctx.params.id).populate('panels');
+    if (comic) {
+      ctx.body = comic;
+    } else {
+      ctx.status = 404;
+      ctx.body = { error: 'Comic not found.' };
+    }
+  } catch (err) {
+    console.error(err);
+    ctx.status = 500;
+    ctx.body = { error: 'An error occurred while fetching the comic.' };
+  }
+});
 
 app
   .use(bodyParser())
