ee0b55441b3202d8e49c3a46b3f305c80e51d8aa
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-04-05 12:53:07 -0700

    BugFix - Remove mongoose.connect arguments; unnecessary in v7

--- db/index.js
+++ db/index.js
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/comics_app';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/ego_gala_comics';
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(MONGODB_URI);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
