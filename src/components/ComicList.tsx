7894fde265dae58a7a808ebb8bb565db3dad6b12
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-04-06 14:39:09 -0700

    Render comic Panels upon clicking a Comic from the ComicList

--- src/components/ComicList.tsx
+++ src/components/ComicList.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { fetchComic } from '../services/comics';
+import { IComic } from 'db/models/Comic';
 
 const List = styled.ul`
   list-style-type: none;
@@ -21,13 +23,33 @@ interface ComicListProps {
 }
 
 const ComicList: React.FC<ComicListProps> = ({ comics }) => {
+  const [selectedComic, setSelectedComic] = useState<IComic | null>(null);
+
+  const handleComicClick = async (comicId: string) => {
+    try {
+      const comic = await fetchComic(comicId);
+      setSelectedComic(comic);
+    } catch (error) {
+      console.error('Error fetching comic:', error);
+    }
+  };
+
   return (
     <List>
       {comics.map((comic) => (
-        <ListItem key={comic._id}>
+        <ListItem key={comic._id} onClick={() => handleComicClick(comic._id)}>
           {comic.title} - {new Date(comic.publication_date).toLocaleDateString()}
         </ListItem>
       ))}
+      {/* Render the selected comic if it exists */}
+      {selectedComic && (
+        <div>
+          {/* Render the comic panels */}
+          {selectedComic.panels.map((panel) => (
+            <img key={panel._id} src={panel.image_url} alt={`Panel ${panel.panel_number}`} />
+          ))}
+        </div>
+      )}
     </List>
   );
 };
