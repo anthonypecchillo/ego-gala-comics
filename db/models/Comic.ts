f722eef69d0d9b071aa17d0b06ae34c56316e851
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-08-03 20:09:21 -0500

    Use IPanel instead of Array<string> while typing Panels in Comic schema

--- db/models/Comic.ts
+++ db/models/Comic.ts
@@ -1,19 +1,24 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
+import { IPanel } from "./Panel";
 
 export interface IComic extends Document {
   title: string;
   category: string;
   description: string;
   publication_date: Date;
-  panels: Array<string>;
+  panels: IPanel[];
 }
 
 const ComicSchema: Schema = new Schema({
   title: { type: String, required: true },
-  category: { type: String, enum: ['diary', 'fantology', 'compendium'], required: true },
+  category: {
+    type: String,
+    enum: ["diary", "fantology", "compendium"],
+    required: true,
+  },
   description: { type: String, required: true },
   publication_date: { type: Date, required: true },
-  panels: [{ type: Schema.Types.ObjectId, ref: 'Panel' }],
+  panels: [{ type: Schema.Types.ObjectId, ref: "Panel" }],
 });
 
-export default mongoose.model<IComic>('Comic', ComicSchema);
+export default mongoose.model<IComic>("Comic", ComicSchema);
