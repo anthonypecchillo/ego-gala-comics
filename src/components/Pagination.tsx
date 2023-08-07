b0cd1f60f7825a227095e0fbdbe07223d210a413
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-08-07 09:36:34 -0500

    Refactor Pagination component to use MUI Pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,32 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const PaginationWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 1rem 0;
-`;
-
-const PageButton = styled.button`
-  background-color: #f5f5f5;
-  border: none;
-  cursor: pointer;
-  margin: 0 0.25rem;
-  padding: 0.5rem;
-  border-radius: 50%;
-  width: 2rem;
-  height: 2rem;
-  line-height: 1rem;
-  text-align: center;
-
-  &:hover {
-    background-color: #e0e0e0;
-  }
-
-  &.active {
-    background-color: #d0d0d0;
-  }
-`;
+import { Pagination as MuiPagination } from '@mui/material';
 
 interface PaginationProps {
   currentPage: number;
@@ -39,18 +12,17 @@ const Pagination: React.FC<PaginationProps> = ({
   totalPages,
   onPageClick,
 }) => {
+  const handleChange = (event: React.ChangeEvent<unknown>, page: number) => {
+    onPageClick(page);
+  };
+
   return (
-    <PaginationWrapper>
-      {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
-        <PageButton
-          key={page}
-          className={currentPage === page ? 'active' : ''}
-          onClick={() => onPageClick(page)}
-        >
-          {page}
-        </PageButton>
-      ))}
-    </PaginationWrapper>
+    <MuiPagination
+      count={totalPages}
+      page={currentPage}
+      onChange={handleChange}
+      color="primary"
+    />
   );
 };
 
