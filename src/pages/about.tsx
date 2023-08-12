650305f25c22210f2f8a661bfe864c5cde83742d
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-08-12 15:26:24 -0700

    Build out basic About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,20 +1,107 @@
-// components/About.tsx
 import React from 'react';
+import Image from 'next/image';
 import { ThemeProvider } from 'styled-components';
 import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-// import Hero from '../components/Hero';
-// import GridSection from '../components/GridSection';
-import NewsletterForm from '../components/NewsletterForm';
 import GlobalStyle from '../styles/GlobalStyle';
 import theme from '../styles/theme';
+
+import {
+  Container,
+  Typography,
+  Box,
+  Link,
+  Divider,
+  Paper,
+} from '@mui/material';
 
 const About: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Navbar />
-      <div>About page</div>
+      <Paper elevation={3} sx={{ p: 2, my: 2, mx: 'auto', maxWidth: 'md' }}>
+        <Container maxWidth="md">
+          <Box
+            mt={4}
+            mb={6}
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+          >
+            <Image
+              src="/kristen_shull.png"
+              alt="Kristen Shull's Artwork"
+              width={480}
+              height={234}
+            />
+            <br />
+            <br />
+            <Typography variant="h4" gutterBottom>
+              Kristen Shull
+            </Typography>
+            <Typography variant="subtitle1" align="center">
+              Comic Artist & Educator in Burlington, Vermont
+            </Typography>
+          </Box>
+
+          <Typography variant="body1" paragraph>
+            Kristen Shull is a comic artist and educator residing in Burlington,
+            Vermont. She is a graduate of the Center for Cartoon Studies masters
+            program class of 2020. Since then, she has been teaching Sequential
+            Art at Champlain College since 2021. Her comics are also featured
+            bi-monthly in the state-wide free newspaper, Seven Days.
+          </Typography>
+
+          <Typography variant="h6" gutterBottom>
+            Self-Published Works
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Her self-published works include Ego Gala, a four-paneled diary
+            comic kept daily for three years that have been compiled into three
+            books (2019, 2020, and 2021).
+          </Typography>
+
+          <Typography variant="h6" gutterBottom>
+            Co-editor of Fantology
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Kristen is also the co-editor of Fantology with friend and fellow
+            CCS alum, Emily Zea.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Fantology is a fantasy comic anthology featuring an average of 16
+            artists per issue with all fantasy comics set in the same world.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Fantology boasts three self-published issues, Issue One: Origins,
+            Issue Two: Flora and Fauna, and Issue Three: Treasure.
+          </Typography>
+
+          <Divider />
+
+          <Box mt={4} mb={4}>
+            <Typography variant="h6" gutterBottom>
+              Connect with Kristen
+            </Typography>
+            <Typography variant="body1">
+              Follow Kristen's comics on{' '}
+              <Link
+                href="https://instagram.com/ego_gala"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Instagram
+              </Link>
+              .
+            </Typography>
+            <Typography variant="body1">
+              For professional inquiries, email{' '}
+              <Link href="mailto:k10.shull@gmail.com">k10.shull@gmail.com</Link>
+              .
+            </Typography>
+          </Box>
+        </Container>
+      </Paper>
     </ThemeProvider>
   );
 };
