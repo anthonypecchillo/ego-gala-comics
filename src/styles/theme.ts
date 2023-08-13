50b0ed5fe5dc9402ddc67e427539d618541ceec1
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-08-13 07:24:40 -0700

    Init MUI theme and add Walter Turncoat and Patrick Hand SC fonts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,24 @@
-// styles/theme.ts
-const theme = {
-  colors: {
-    primary: '#333',
-    secondary: '#a0a',
+import { createTheme } from '@mui/material/styles';
 
-    darkGrey: '#c0c0c0',
-    hoverGrey: '#e0e0e0',
-  },
+const customColors = {
+  primary: '#333',
+  secondary: '#a0a',
+  darkGrey: '#c0c0c0',
+  hoverGrey: '#e0e0e0',
 };
+
+const theme = createTheme({
+  palette: {
+    // primary: {
+    //   main: customColors.primary,
+    // },
+    // secondary: {
+    //   main: customColors.secondary,
+    // },
+  },
+  typography: {
+    fontFamily: '"Walter Turncoat", "Patrick Hand SC", sans-serif',
+  },
+});
 
 export default theme;
