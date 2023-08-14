e580d61f6a7f1f6a2a9db719e2e73d2fd68c160c
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-08-14 14:02:24 -0700

    Simplify _document.tsx by removing support for unused @emotion/css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,7 @@ import Document, {
   NextScript,
   DocumentContext,
 } from 'next/document';
-import createEmotionServer from '@emotion/server/create-instance';
-import { cache } from '@emotion/css';
 import { ServerStyleSheet } from 'styled-components';
-
-const { extractCritical } = createEmotionServer(cache);
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -25,20 +21,14 @@ export default class MyDocument extends Document {
         });
 
       const initialProps = await Document.getInitialProps(ctx);
-      const emotionStyles = extractCritical(initialProps.html);
-      initialProps.html = emotionStyles.html;
-
       return {
         ...initialProps,
-        styles: [
-          ...React.Children.toArray(initialProps.styles),
-          styledComponentsSheet.getStyleElement(),
-          <style
-            data-emotion={`css ${emotionStyles.ids.join(' ')}`}
-            key="emotion-style-tag"
-            dangerouslySetInnerHTML={{ __html: emotionStyles.css }}
-          />,
-        ],
+        styles: (
+          <>
+            {initialProps.styles}
+            {styledComponentsSheet.getStyleElement()}
+          </>
+        ),
       };
     } finally {
       styledComponentsSheet.seal();
