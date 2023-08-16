2e2aca6ff8fbc5a925631645837bb3682250e9e0
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-08-15 23:53:17 -0700

    Experiment with social links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,7 +38,19 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head title="Ego Gala Comics">
+          <meta name="description" content="Incredibly satisfying illustrations." />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://ego-gala-comics.vercel.app/" />
+          <meta property="og:title" content="Ego Gala Comics (Social Media Title)" />
+          <meta property="og:description" content="Incredibly satisfying illustrations. (Social Media Description)" />
+          <meta property="og:image" content="https://ego-gala-comics.vercel.app/_next/image?url=%2Fkristen_shull.png&w=1080&q=75" />
+          <meta property="og:image:alt" content="An image should have been here..." />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:site_name" content="Ego Gala Comics" />
+        </Head>
         <body>
           <Main />
           <NextScript />
