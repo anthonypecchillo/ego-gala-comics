c172f3215c3497dd43855da26f90eadcc66d229e
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-08-21 10:35:46 -0700

    Init CMS page

---
+++ src/pages/cms.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { IComic } from '../db/models/Comic';
+import { IPanel } from '../db/models/Panel';
+
+const CMS: React.FC = () => {
+  return (
+    <div>
+      <h1>CMS</h1>
+    </div>
+  );
+}
+
+export default CMS;
