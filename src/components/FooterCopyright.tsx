1689ae9642b1a4bde850640e359c3c6435cbbd0a
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-09-13 14:05:17 -0700

    Add key to each item in dynamically generated list

--- src/components/FooterCopyright.tsx
+++ src/components/FooterCopyright.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { useMediaQuery, useTheme } from '@mui/material';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
@@ -17,11 +18,11 @@ const FooterCopyright = () => {
 
         {isMobile &&
           FOOTER.COPYRIGHT_TEXT.split(', ').map((segment, index) => (
-            <>
+            <Box component="span" key={segment}>
               {segment}
               {index < FOOTER.COPYRIGHT_TEXT.split(', ').length - 1 && ','}
               <br />
-            </>
+            </Box>
           ))}
       </Typography>
     </Grid>
