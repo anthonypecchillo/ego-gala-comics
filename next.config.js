3ea1123ef17522d3521467e36820055f54cbc72f
Anthony Pecchillo <anthonypecchillo@gmail.com>
2024-01-21 21:14:57 -0800

    remove unused hostname patterns from next.config.js

--- next.config.js
+++ next.config.js
@@ -5,12 +5,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'i0.wp.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'ego-gala-comics.s3.us-east-1.amazonaws.com',
         port: '',
         pathname: '/**',
@@ -18,24 +12,6 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'ego-gala-comics.s3.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.vecteezy.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.brosix.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
         port: '',
         pathname: '/**',
       },
