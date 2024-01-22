99d359b12e5c053e11496f101b43fabca5207dec
Anthony Pecchillo <anthonypecchillo@gmail.com>
2024-01-22 00:12:36 -0800

    Make hero image dynamic based on screen width

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,25 +3,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HeroContainer = styled.div`
-  background-image: url('hero3.jpg');
-  // background-image: url('hero.png');
-  // background-color: #b4abd0;
-  // background: radial-gradient(
-  //   circle at center,
-  //   #f7f6fa,
-  //   #ebe8f1,
-  //   #d9d4e7,
-  //   #8f82a9,
-  //   #4b3f72,
-  //   #352b56,
-  //   #1f2041
-  // );
-
   background-size: cover;
-  // background-position: center;
-  background-position: top left;
+  background-position: center;
+  // background-position: top left;
   width: 100%;
-  height: 30.314vw;
+
+  // Default background image and height for mobile devices
+  background-image: url('Banner_mobile.jpg');
+  height: 150.5vw;
+
+  @media (min-width: 600px) {
+    background-image: url('Banner_tablet.jpg');
+    height: 74.5vh;
+  }
+
+  @media (min-width: 900px) {
+    background-image: url('Banner_desktop.jpg');
+    height: 45.5vw;
+    // height: 30.314vw;
+  }
 `;
 
 const Hero = () => {
