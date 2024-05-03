a471bf9d632d0699241ba451be3831999f6ac342
Anthony Pecchillo <anthonypecchillo@gmail.com>
2024-05-02 20:14:41 -0500

    Fix 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,36 +1,68 @@
 import React from 'react';
 
+import { useMediaQuery, useTheme } from '@mui/material';
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Custom404() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  const navLinkStyles = {
+    color: theme.palette.common.white,
+    '&:hover': {
+      color: theme.palette.secondary.main,
+    },
+  };
+
+  const navLinkTextStyles = {
+    color: theme.palette.common.white,
+    marginRight: '12px',
+    cursor: 'pointer',
+    '&:hover': {
+      color: theme.palette.secondary.main,
+    },
+  };
+
   return (
     <Paper
       elevation={3}
       sx={{
         p: 2,
-        my: 5,
-        mx: 'auto',
-        maxWidth: 'md',
+        mt: 12,
+        mb: 6,
+        mx: isMobile ? '16px' : 'auto',
+        maxWidth: '700px',
         textAlign: 'center',
+        backgroundColor: 'black',
       }}
     >
-      <h1>Oh no, 404!</h1>
       <Image
-        src="https://ego-gala-comics.s3.us-east-1.amazonaws.com/404.jpg"
+        src="https://ego-gala-comics.s3.us-east-1.amazonaws.com/public/404.jpg"
         alt="404"
         sizes="100vw"
         style={{
           width: '100%',
           height: 'auto',
-          maxWidth: '600px',
+          maxWidth: '500px',
+          padding: '0px 16px',
         }}
         width={5412}
         height={7517}
       />
-      <p>This is probably your fault.</p>
-      <Link href="/">Go back home?</Link>
+      <br />
+      <br />
+      <Typography variant="body1" color="white">
+        This is probably your fault.
+      </Typography>
+      <br />
+      <Link href="/" style={navLinkStyles}>
+        <Typography variant="body1" sx={navLinkTextStyles}>
+          Go back home?
+        </Typography>
+      </Link>
       <br />
       <br />
     </Paper>
